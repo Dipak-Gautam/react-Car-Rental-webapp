0d8a94fa739002e1ce708286675374a321ab097b
Dipak-Gautam <anjangautam095@gmail.com>
2024-03-13 11:05:29 +0545

    darkmode complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,25 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import { useEffect, useState } from "react";
+
 import Navbar from "./components/navbar/Navbar";
 
 function App() {
+  // dark mode
+  const [theme, settheme] = useState(
+    localStorage.getItem("theme") ? localStorage.getItem("theme") : "light"
+  );
+  const element = document.documentElement;
+  useEffect(() => {
+    if (theme === "dark") {
+      element.classList.add("dark");
+      localStorage.setItem("theme", "dark");
+    } else {
+      element.classList.remove("dark");
+      localStorage.setItem("theme", "light");
+    }
+  }, [theme]);
   return (
     <>
-      <Navbar></Navbar>
+      <Navbar theme={theme} settheme={settheme} />
     </>
   );
 }
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -2,9 +2,9 @@ import React from "react";
 import { MdLightMode } from "react-icons/md";
 import { BsMoonStars } from "react-icons/bs";
 
-const Navbar = () => {
+const Navbar = ({ theme, settheme }) => {
   return (
-    <nav className="shadow-sm">
+    <nav className="shadow-md  dark:bg-black dark:text-white duration-500">
       <div className="container">
         <div className="flex justify-between  items-center ">
           <div>
@@ -14,32 +14,32 @@ const Navbar = () => {
             <ul className="flex gap-6 items-center text-xl py-2 ">
               <li>
                 <a
-                  className=" text-slate-800  py-1 no-underline  hover:border-b-2 hover:text-primary transition-colors transition-duration-500 font-medium"
-                  href=""
+                  className=" text-slate-800  py-1 no-underline  hover:border-b-2 hover:text-primary transition-colors transition-duration-500 font-medium dark:text-white dark:hover:text-primary"
+                  href="#"
                 >
                   Home
                 </a>
               </li>
               <li>
                 <a
-                  className="text-slate-800  py-1 no-underline  hover:border-b-2 hover:text-primary transition-colors transition-duration-500 font-medium"
-                  href=""
+                  className="text-slate-800  py-1 no-underline  hover:border-b-2 hover:text-primary transition-colors transition-duration-500 font-medium dark:text-white dark:hover:text-primary"
+                  href="#"
                 >
                   Cars
                 </a>
               </li>
               <li>
                 <a
-                  className="text-slate-800  py-1 no-underline  hover:border-b-2 hover:text-primary transition-colors transition-duration-500 font-medium"
-                  href=""
+                  className="text-slate-800  py-1 no-underline  hover:border-b-2 hover:text-primary transition-colors transition-duration-500 font-medium dark:text-white  dark:hover:text-primary"
+                  href="#"
                 >
                   About
                 </a>
               </li>
               <li>
                 <a
-                  className="text-slate-800  py-1 no-underline  hover:border-b-2 hover:text-primary transition-colors transition-duration-500 font-medium"
-                  href=""
+                  className="text-slate-800  py-1 no-underline  dark:text-white hover:border-b-2 hover:text-primary transition-colors transition-duration-500 font-medium dark:hover:text-primary"
+                  href="#"
                 >
                   Booking
                 </a>
@@ -47,8 +47,17 @@ const Navbar = () => {
             </ul>
           </div>
           <div>
-            <MdLightMode />
-            <BsMoonStars />
+            {theme === "dark" ? (
+              <MdLightMode
+                onClick={() => settheme("light")}
+                className="p-2 text-4xl"
+              />
+            ) : (
+              <BsMoonStars
+                onClick={() => settheme("dark")}
+                className="p-2 text-4xl"
+              />
+            )}
           </div>
         </div>
       </div>
